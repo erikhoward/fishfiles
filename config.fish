e527b3527677cdbed3123e7e55ad3153adf8b8d7
erikhoward <erikhoward@pm.me>
2020-11-24 09:47:39 -0800

    feat: updating PATH for nim support

--- config.fish
+++ config.fish
@@ -18,7 +18,7 @@ else
     set -gx LC_CTYPE en_US.UTF-8
 
     # global paths
-    set -gx PATH /usr/local/bin $HOME/bin /usr/local/go/bin /usr/bin /bin /usr/sbin /sbin $HOME/bin $HOME/.local/bin $PATH
+    set -gx PATH /usr/local/bin $HOME/bin $HOME/.nimble/bin /usr/local/go/bin /usr/bin /bin /usr/sbin /sbin $HOME/bin $HOME/.local/bin $PATH
     set -gx MANPATH /usr/share/man /usr/local/man /usr/local/share/man $MANPATH
     if test -d $HOME/.local/share/man
         set -gx MANPATH $MANPATH $HOME/.local/share/man
